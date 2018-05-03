293acdaee1af7c1beab8b61be988a09f9259ee87
Anwesh43 <anweshthecool0@gmail.com>
2018-05-03 08:22:33 +0530

    adding Animator class

--- linetoeview/src/main/java/com/example/linetoeview/LineToEView.kt
+++ linetoeview/src/main/java/com/example/linetoeview/LineToEView.kt
@@ -49,6 +49,35 @@ class LineToEView (ctx : Context) : View(ctx) {
                 startcb()
             }
         }
+    }
+
+    data class Animator (var view : View, var animated : Boolean = false) {
+
+        fun animate(updatecb : () -> Unit) {
+            if (animated) {
+                updatecb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch (ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
 
     }
 }
