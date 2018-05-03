06b547a9eca29924d8b79d14ac1c7323fb0489c3
Anwesh43 <anweshthecool0@gmail.com>
2018-05-03 08:53:12 +0530

    adding fullScreen support

--- app/src/main/java/com/example/anweshmishra/kotlinlinetoeview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinlinetoeview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.anweshmishra.kotlinlinetoeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.linetoeview.LineToEView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LineToEView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
